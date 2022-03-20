c931559dbdea9c458b022588a08cd0d69a2967ad
LukaszMiazek <100946540+LukaszMiazek@users.noreply.github.com>
2022-03-20 18:33:39 +0100

    Add files via upload

---
+++ mag/kadra-zmien.php
@@ -0,0 +1,59 @@
+<?php
+ini_set( 'display_errors', 'Off' ); 
+?>
+<br>
+<br>
+<b class="nag"></b>
+<br>
+<br>
+<?php
+session_start();
+//print_r($_POST);
+
+$conn = mysqli_connect("localhost", "root", "", "magazyn");
+
+if (mysqli_connect_errno()) {
+ echo "Błąd połączenia nr: " . mysqli_connect_errno();
+ echo "Opis błędu: " . mysqli_connect_error();
+ exit();
+}
+
+mysqli_query($conn, 'SET NAMES utf8');
+mysqli_query($conn, 'SET CHARACTER SET utf8');
+mysqli_query($conn, "SET collation_connection = utf8_polish_ci");
+
+$idu = $_POST['idu'];
+
+if (isset ($_POST['upr']))
+{
+	echo 'x';
+	if ($_POST['upr']==1) $result = mysqli_query($conn, "UPDATE `pracownik` SET `STANOWISKO` = 'MAGAZYNIER' WHERE `ID_PRACOWNIKA` = ".$idu);
+	else if ($_POST['upr']==2) $result = mysqli_query($conn, "UPDATE `pracownik` SET `STANOWISKO` = 'KSIEGOWY' WHERE `ID_PRACOWNIKA` = ".$idu);
+}
+
+if (isset ($_POST['blok']))
+{
+	$blok=$_POST['blok'];
+	
+	echo $blok;
+	echo '<br>';
+	echo $idu;
+	
+	if($blok == 0) 
+	{
+		$result = mysqli_query($conn, "UPDATE `pracownik` SET `BLOKADA` = '1' WHERE `ID_PRACOWNIKA` = ".$idu);
+		
+		echo "UPDATE pracownik SET BLOKADA = '1' WHERE 'ID_PRACOWNIKA' = ".$idu;
+	}
+	else 
+	{
+	$result = mysqli_query($conn, "UPDATE pracownik SET `BLOKADA` = '0' WHERE `ID_PRACOWNIKA` = ".$idu);
+	
+	echo "UPDATE pracownik SET BLOKADA = '0' WHERE 'ID_PRACOWNIKA' = ".$idu;
+	}
+}
+
+	header ('location:kadra.php?edit='.$usr);
+	exit;
+?>
+
