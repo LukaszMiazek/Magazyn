13433369617f51b24ae3d329486e13204a90eaa3
Jakub Jankowski <100997301+fly299@users.noreply.github.com>
2022-05-16 00:39:30 +0200

    Update koszyk.php

--- mag/koszyk.php
+++ mag/koszyk.php
@@ -2,13 +2,21 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>sklep</title>
-	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<title>Koszyk</title>
+	<link rel="stylesheet" type="text/css" href="style_koszyk.css">
+	<meta name="viewport"  content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"/> 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
+ 
 </head>
 <body>
+<div class="container">
+<div class="nav">
 <?php
 session_start();
+echo '<div class="wyloguj">';
 echo 'Zalogowno jako '.$_SESSION['login'];
 ?>
 <form action="klient.php" method="post">
@@ -17,11 +25,12 @@ echo 'Zalogowno jako '.$_SESSION['login'];
 	<button type="submit">Wyloguj się</button>
 
 </form>
+</div>
 <?php
-echo '<br><br>';
-echo '<a href="Sklep.php">Sklep</a>';
-echo '<br>';
-echo '<br>';
+echo '<div class="sklep">';
+echo '<a href="Sklep.php"><i class="fa-solid fa-shop"></i> Sklep</a>';
+echo '</div>';
+echo '</div>';
 
 require 'rb-mysql.php';
 R::setup( 'mysql:host=localhost;dbname=magazyn','root', '' );
@@ -52,55 +61,72 @@ if (isset ($_POST['zid']))
 $zamis = R::findOne('zamowienie', 'id_klienta = ? AND status = 0', [$_SESSION['id']]);
 
 //echo $zamis->id;
-
+echo '<div class="panel">';
 if (empty($zamis)) 
 {
-	echo 'Koszyk jest pusty';
+	echo '<h1>Koszyk jest pusty</h1>	';
 }
 else
 {
-	echo 'Zawartość koszyka: ';
+	echo '<h1>Zawartość koszyka </h1>';
 	echo '<br>';
-	
 	$tw = R::find('sklad', ' id_zam = ?', [$zamis->id] );
 	$suma=0;
 	foreach ($tw as $towar){
 		//echo $towar->towar;
 		
+		echo '<div class="towar">';
 		$t = R::findOne( 'towar', 'id=?', [$towar->towar] );
+			
+		echo '<img src = "Zdjecia/';
+		echo $t->id; 
+		echo '.png " width="50" height="50" alt=" " ></a>';
 
-		echo $towar->ilosc;
-		echo 'x ';
+		echo'<h2>';
 		echo $t['nazwa'];
-		echo ' ';
+		echo'</h2>';
+		echo'<h3>';
+		echo $towar->ilosc;
+		echo'</h3>';
+		echo'<h3>';
 		echo $t['cena'];
+		echo ' zł';
+		echo'</h3>';
+		
 		
 		?>
 		<form action="" method="post">
 		<input type="hidden" name="zid" value="<?php echo $zamis->id?>"required>
 		<input type="hidden" name="tid" value="<?php echo $towar->towar?>"required>
-		<input type="submit" value="Usuń">
+		<button type="submit" class="fa-solid fa-xmark">
 		</form>
 		<?php
 		
 		
 		$suma=$suma+$t['cena']*$towar->ilosc;
+		echo'</div>';
 	}
-	
 	echo '<br>';
-	echo 'Suma: ';
+	echo '<div class="suma">';
+	echo 'Suma ';
 	echo number_format($suma, 2);
+	echo ' zł';
 	
 	?>
 		<form action="" method="post">
 		<input type="hidden" name="zloz" value="<?php echo $zamis->id; ?>" required>
 		<input type="submit" value="Złóż zamówienie">
 		</form>
+</div>
+</div>
 	<?php
 }
 
 echo '<br>';
 echo '<br>';
+
+	
+echo '<div class="podsumowanie">';
 
 $zaakt= R::findAll('zamowienie', 'id_klienta = ? AND status != 0', [$_SESSION['id']]);
 //echo $zamis->id;
@@ -110,15 +136,15 @@ if (empty($zaakt))
 }
 else
 {
-	echo 'Twoje zamówienia:';
-	echo '<br>';
-	echo '<br>';
+	echo '<h1>Twoje zamówienia</h1>';
 	
 	foreach ($zaakt as $zam)
 	{
+		echo '<div class="historia">';
+		echo '<h3>';
 		echo 'Zamówienie numer: ';
 		echo $zam->id;
-		echo '<br>';
+		echo '</h3>';
 		echo 'Data złożenia zamówienia: ';
 		echo $zam->data;
 		echo '<br>';
@@ -159,14 +185,18 @@ else
 		}
 		
 		echo '<br>';
+		echo '<h3>';
 		echo 'Suma: ';
 		echo number_format($suma, 2);
+		echo ' zł';
+		echo '</h3>';
 		echo '<br>';
-		echo '<br>';
-		
+		echo '</div>';
 	}
 }
+	echo '</div>';
 
 ?>
+</div>
 </body>
 </html>
