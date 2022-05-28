984ddf48c61c2a28a0b45ae9c7b3d756eb970a90
Jakub Jankowski <100997301+fly299@users.noreply.github.com>
2022-05-28 07:38:58 +0200

    Update wysylka.php

--- mag/wysylka.php
+++ mag/wysylka.php
@@ -3,13 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css"/>
+    <link rel="stylesheet" href="./style_wysylka.css"/>
     <title>Wysyłka</title>
+	<meta name="viewport"  content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"/> 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
+<div class="container">
+<div class="nav">
 <?php
 session_start();
 
+echo '<div class="logo">';
+echo '<i class="fa-solid fa-coins"></i>';
+echo '<h2>magazyn/wysyłka</h2>';
+echo '</div>';
+echo '<div class="nav_panel">';
 	echo '<m1>Zalogowany jest ';
 	echo $_SESSION['stanowisko'];
 	echo ' ';
@@ -20,10 +32,13 @@ session_start();
 <form action="index.php" method="post">
 
 	<input type="hidden" name="wlog"	required>
-	<button type="submit">Wyloguj się</button>
+	<button type="submit"><i class="fa-solid fa-arrow-right-from-bracket"></i> Wyloguj </button>
 	</div>
 </form>	
 <?php
+		echo '</div>';
+
+	
 
 require 'rb-mysql.php';
 R::setup( 'mysql:host=localhost;dbname=magazyn','root', '' );
@@ -47,19 +62,20 @@ else if (isset ($_POST['numzam']) )
 	if($zamis['status'] != 3)
 	{
 		echo '<div class="info">';
-		echo 'To zamówienie NIE jest kompletne!';
+		echo '<h2>To zamówienie NIE jest kompletne!</h2>';
 		echo '<a href="wysylka.php"> Powrót </a>';
 		echo '</div>';
 	}
 	else
 	{
+		echo '<div class="panel">';
 		?>
-		dane do wysyłki:
-		<br>
-		Miejscowość: <?php echo $zamis['miejscowosc']; ?> <br>
-		Kod Pocztowy: <?php echo $zamis['kod_pocztowy']; ?> <br>
-		Ulica: <?php echo $zamis['ulica']; ?> <br>
-		Numer domu: <?php echo $zamis['numer_domu']; ?> <br> <br>
+		<h1>Dane do wysyłki</h1>
+		
+	<label>Miejscowość:</label> <?php echo $zamis['miejscowosc']; ?> 
+		<label>Kod Pocztowy:</label> <?php echo $zamis['kod_pocztowy']; ?> 
+		<label>Ulica:</label> <?php echo $zamis['ulica']; ?>
+		<label>Numer domu:</label> <?php echo $zamis['numer_domu']; ?> 
 		
 		<form action="" method="post">
 			<input type="hidden" name="wys" value="<?php echo $zamis['id']; ?>" required>
@@ -68,17 +84,21 @@ else if (isset ($_POST['numzam']) )
 		
 		<a href="wysylka.php"> Powrót </a>
 		<?php
+		echo '</div>';
 	}
 }
 else 
 {
-	echo '<br>';
 	echo '<div class="info">';
 	?>	
 	<form action="" method="post">
-	Numer kosza:
-		<input type="number" name="numzam" required>
-		<input type="submit" value="Zapakuj">
+	<label>Numer kosza:</label>
+	<div class="info_number">
+	<input type="number" name="numzam" required>
+	</div>
+	<div class="info_button">
+	<input type="submit" value="Zapakuj">
+		</div>
 	</form>
 	<?php
 	echo '</div>';
@@ -88,6 +108,7 @@ else
 
 	
 ?>
+</div>
 </body>
 </html>
 
